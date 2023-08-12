7f45065b2010b123d58fc72b54dd03d6d0fd5755
Brandon.Bustos <brandon.bustos@olsoftware.com>
2023-08-11 19:45:12 -0500

    test: agregando test de metodo concatenacion

---
+++ StringManipulation.Test/StringOperationTest.cs
@@ -0,0 +1,25 @@
+﻿namespace StringManipulation.Test
+{
+	public class StringOperationTest
+	{
+		// Que_Vamos_A_Probar
+		// El_Escenario
+		// Lo_que_debe_arrojar
+
+		[Fact]
+		public void ConcatenateStrings()
+		{
+			// Arange
+			// Se configura los parametros de entrada de nuestra prueba unitaria
+			var strOperations = new StringOperations();
+
+			// Act
+			// Se ejecuta el metodo a probar de nuestra prueba unitaria
+			var result = strOperations.ConcatenateStrings("Hello","Brandon");
+
+			// Assert
+			// Se verifica los datos de retorno de nuestro metodo probado en la prueba unitaria
+			Assert.Equal("Hello Brandon", result);
+		}
+	}
+}
