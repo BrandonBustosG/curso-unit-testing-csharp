80e254976fafe6706aebe7d19f05bfcbe1403d78
Brandon.Bustos <brandon.bustos@olsoftware.com>
2023-08-14 18:45:58 -0500

    test: se agregan pruebas usando Theory, InLineData y atributo Skip

--- StringManipulation.Test/StringOperationTest.cs
+++ StringManipulation.Test/StringOperationTest.cs
@@ -6,7 +6,7 @@
 		// El_Escenario
 		// Lo_que_debe_arrojar
 
-		[Fact]
+		[Fact(Skip = "Esta prueba no es valida en este momento, Ticket-001")]
 		public void ConcatenateStrings()
 		{
 			// Arange
@@ -15,7 +15,7 @@
 
 			// Act
 			// Se ejecuta el metodo a probar de nuestra prueba unitaria
-			var result = strOperations.ConcatenateStrings("Hello","Brandon");
+			var result = strOperations.ConcatenateStrings("Hello", "Brandon");
 
 			// Assert
 			// Se verifica los datos de retorno de nuestro metodo probado en la prueba unitaria
@@ -50,17 +50,72 @@
 			Assert.False(result);
 		}
 
-		[Fact]
-		public void RemoveWhitespace()
+		[Theory]
+		[InlineData("He ll o", "Hello")]
+		[InlineData("c a r", "car")]
+		public void RemoveWhitespace(string word, string expected)
 		{
 			// Arrange
 			var strOperations = new StringOperations();
 
 			// Act
-			var result = strOperations.RemoveWhitespace("H e l l o");
+			var result = strOperations.RemoveWhitespace(word);
 
 			// Assert
-			Assert.Equal("Hello", result);
+			Assert.Equal(expected,result);
+		}
+
+		[Fact]
+		public void QuantintyInWords()
+		{
+			// Arrange
+			var strOperations = new StringOperations();
+
+			// Act
+			var result = strOperations.QuantintyInWords("cat", 10);
+
+			// Assert
+			Assert.StartsWith("ten", result);
+			Assert.Contains("cat", result);
+		}
+
+		[Fact]
+		public void TruncateStringException()
+		{
+			// Arrange
+			var strOperations = new StringOperations();
+
+			// Assert
+			Assert.ThrowsAny<ArgumentNullException>(() => strOperations.GetStringLength(null));
+		}
+
+		[Fact]
+		public void TruncateString()
+		{
+			// Arrange
+			var strOperations = new StringOperations();
+
+			// Act
+			var result = strOperations.TruncateString("hello".ToUpper(), 2);
+
+			// Assert
+			Assert.Equal("he".ToUpper(), result);
+		}
+
+		[Theory]
+		[InlineData("V",5)]
+		[InlineData("III", 3)]
+		[InlineData("X", 10)]
+		public void FromRomanToNumber(string romanNumber, int expected)
+		{
+			// Arrange
+			var strOperations = new StringOperations();
+
+			// Act
+			var result = strOperations.FromRomanToNumber(romanNumber);
+
+			// Assert
+			Assert.Equal(expected, result);
 		}
 
 	}
